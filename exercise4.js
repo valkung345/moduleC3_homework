62f1bad7cbfb4d63b418f888d48c2adac70201bf
valkung345 <121906475+valkung345@users.noreply.github.com>
2023-07-18 13:21:00 +0500

    exercise4.js

--- exercise4.js
+++ exercise4.js
@@ -1,1 +1,54 @@
+// Задание 4
+// Реализовать следующее консольное приложение подобно примеру, который разбирался в видео. Реализуйте его на прототипах.
+// Определить иерархию электроприборов. Включить некоторые в розетку.
+// Посчитать потребляемую мощность. 
+// Таких приборов должно быть, как минимум, два (например, настольная лампа и компьютер).
+// Выбрав прибор, подумайте, какими свойствами он обладает.
 
+function ElectricalAppliance(name, power) {
+    this.name = name;
+    this.power = power;
+    this.isPlugged = false;
+}
+
+ElectricalAppliance.prototype.plugIn = function() {
+    console.log(this.name + " is plugged!");
+    this.isPlugged = true;
+};
+
+ElectricalAppliance.prototype.unplug = function() {
+    console.log(this.name + " is unplugged!");
+    this.isPlugged = false;
+};
+
+function Lamp(name, brand, power, bulbType) {
+    this.name = name;
+    this.brand = brand;
+    this.power = power;
+    this.bulbType = bulbType;
+    this.isPlugged = true;
+}
+
+Lamp.prototype = new ElectricalAppliance();
+
+function Computer(name, brand, power, type, functionality) {
+    this.name = name;
+    this.brand = brand;
+    this.power = power;
+    this.type = type;
+    this.functionality = functionality;
+    this.isPlugged = false;
+}
+
+Computer.prototype = new ElectricalAppliance();
+
+const tableLamp = new Lamp("Table lamp", "Xiaomi", 5, "LED");
+
+const homePC = new Computer("Table PC", "Intel", 120, "stationary", "for work");
+
+tableLamp.unplug();
+
+homePC.plugIn();
+
+console.log(homePC)
+console.log(tableLamp)
